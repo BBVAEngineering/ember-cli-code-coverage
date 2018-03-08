25d8d009abef6cdd9c258e961bc36dcd8346106f
Eduardo Tarancón Delgado <eduardo.tarancon.contractor@bbva.com>
2018-03-08 11:36:55 +0100

    [MCH][DB-85440][fix] fix editorconfig

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,13 @@
 'use strict';
 
 module.exports = {
-  root: true,
-  parserOptions: {
-    ecmaVersion: 6
-  },
-  extends: 'eslint-config-bbva',
-  env: {
-    browser: true,
-    node: true
-  }
+	root: true,
+	parserOptions: {
+		ecmaVersion: 6
+	},
+	extends: 'eslint-config-bbva',
+	env: {
+		browser: true,
+		node: true
+	}
 };
